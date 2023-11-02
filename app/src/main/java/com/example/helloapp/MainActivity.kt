65df4e7958b0029b4c70f6e6111c7e0632000cc6
Alekseev <igor.alekseev.20024@gmail.com>
2023-11-02 22:28:18 +0300

    FirstCommit

---
+++ app/src/main/java/com/example/helloapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.helloapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.helloapp.ui.theme.HelloAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text(text = "Hello METANIT.COM!",
+                style = TextStyle(
+                    fontSize = 22.sp
+                )
+            )
+        }
+    }
+}
+
